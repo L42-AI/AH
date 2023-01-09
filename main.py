68eb46c1aede7cd94e895d57bb7df24083d49433
Jacob Groot <jacobgroot79@gmail.com>
2023-01-09 15:43:37 +0100

    nieuwe main

---
+++ main.py
@@ -0,0 +1,37 @@
+from classes import *
+from data import COURSES, ROOMS, STUDENT_COURSES
+
+'''
+**UPLOADEN**
+git add main.py
+git commit -m "nieuwe main" 
+git push origin main
+'''
+# Jacob's test
+def assign(COURSES, ROOMS, STUDENT_COURSES):
+    '''fill in lists and dictionaries with instances'''
+    course_list = []
+    student_list = []
+    rooms = {}
+
+    for _, course in COURSES.iterrows():
+
+        # create an instance for every course
+        course_list.append(Course(course))
+
+    for _, student in STUDENT_COURSES.iterrows():
+
+        # fill in the list with student instances
+        student_list.append(Student(student))
+
+    for _, room in ROOMS.iterrows():
+
+        # fill the dict with room numbers, and their occupational status
+        rooms[room['Zaalnummber']] = 'free'
+
+    return course_list, student_list, rooms
+
+course_list, student_list, rooms = assign(COURSES, ROOMS, STUDENT_COURSES)
+
+for student in student_list:
+    print(student.f_name)
