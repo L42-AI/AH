f7b8c01802ce3f2b6655c942b945d6f1648474ca
Luka Zaitch <117086277+L42-AI@users.noreply.github.com>
2023-01-09 13:37:45 +0100

    Add files via upload

--- classes.py
+++ classes.py
@@ -3,28 +3,50 @@
 '''
 
 class Student():
-    def __init__(self, last_name, first_name, std_numb, course):
+    def __init__(self, last_name, first_name, id, courses):
 
-        # course will be a list
-        self.course = course
-        self.last_name = last_name
-        self.first_name = first_name
-        self.std_numb = std_numb
+        self.name = f'{first_name} {last_name}'
+        self.id = id
+
+        # Set a list for the courses the student takes
+        self.courses = []
 
         # here the results will be stored
         self.enrollment = {}
+
+        # Run add_courses to add all courses to each student
+        self.add_courses(courses)
+
+    def add_courses(self, courses):
+        """ Assign all the courses to the student and set the enrollment dictionary """
+
+        # For each course:
+        for course in courses:
+
+            # Add courses to the courses list
+            self.courses.append(course)
+
+            # Set the courses as keys in the enrolment dictionary
+            self.enrollment[course] : 999
+
+    def assign_tutorial_groups(self):
+        """ Assign the student's tutorial group per course """
+        pass
+
 
 
 
 class Course():
     # *arg toevoegen
-    def __init__(self, name, lectures, tutorials, max_std, practical, estimated_std):
-        self.name = name
-        self.lectures = lectures
-        self.tutorials = tutorials
-        self.max_std = max_std
-        self.practical = practical
-        self.estimated_std = estimated_std
+    def __init__(self, name, lectures, tutorials, max_std, practicals, estimated_std):
+        self.name = name = str
+
+        self.lectures = lectures = int
+        self.tutorials = tutorials = int
+        self.practicals = practicals = int
+        self.max_std = max_std = int
+
+        self.estimated_std = estimated_std = int
 
 
 
