b395840d672ad15238329a869aadca66183411a2
Luka Zaitch <lzaitch@gmail.com>
2023-01-09 16:25:03 +0100

    Verwijder meerdere tutorials

--- classes.py
+++ classes.py
@@ -1,7 +1,3 @@
-'''a student will have 'course' and 'name' as an argument. 
-    enrollment will be the course and the assigned tutorial
-'''
-
 class Student():
     def __init__(self, info):
 
@@ -12,10 +8,6 @@ class Student():
         # create a list with courses that a student follows
         self.courses = [course for course in info if str(course).startswith("Vak") and course != 'Nan']
 
-        # here the results will be stored
-        self.enrollment = {}
-
-
     def add_courses(self, courses):
         """ Assign all the courses to the student and set the enrollment dictionary """
 
@@ -25,25 +17,20 @@ class Student():
             # Add courses to the courses list
             self.courses.append(course)
 
-            # Set the courses as keys in the enrolment dictionary
-            self.enrollment[course] : 999
-
     def assign_tutorial_groups(self):
         """ Assign the student's tutorial group per course """
         pass
 
 
-
-
 class Course():
     # *arg toevoegen
     def __init__(self, course):
-        self.name = course['Vak'] 
+        self.name = course['Vak']
         self.lectures = course['#Hoorcolleges']
-        self.tutorials = course['#Werkcolleges'] 
-        self.max_std = course['Max. stud. Werkcollege'] 
+        self.tutorials = course['#Werkcolleges']
+        self.max_std = course['Max. stud. Werkcollege']
         self.practica = course['#Practica']
-        self.max_std_practica = course['Max. stud. Practicum'] 
+        self.max_std_practica = course['Max. stud. Practicum']
         self.expected = course['Verwacht']
 
 
