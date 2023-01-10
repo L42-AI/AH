fc8c356e791d692ce89c80e9f4228c39708c4cde
Jacob Groot <70103333+jacobgroot@users.noreply.github.com>
2023-01-10 12:34:28 +0100

    main calls schedule_fill

--- main.py
+++ main.py
@@ -9,5 +9,28 @@ git commit -m "nieuwe main"
 git push origin Jacob
 '''
 
-
 course_list, student_list, rooms = assign(COURSES, ROOMS, STUDENT_COURSES)
+
+# THIS CAN PROBABLY BE DELETED LATER BUT IT IS NICE FOR OVERVIEW #
+## first go over every course and see how many timeslots are needed
+### timeslot needed for every lecture, tut, pract
+time_slot_count = 0
+for course in course_list:
+    time_slot_count += course.lectures + course.tutorials + course.practica
+print(time_slot_count)
+
+# here 
+roster = Roster(rooms)
+for course in course_list:
+    for i in range(course.lectures):
+        roster.fill_schedule(course.name)
+    for i in range(course.tutorials):
+        roster.fill_schedule(course_name)
+    for i in range(course.practica):
+        roster.fill_schedule(course_name)
+
+print(roster.schedule)
+
+    
+
+
