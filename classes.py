23819d0e092f21bffb9d87ddb612842339c778c9
Jacob Groot <70103333+jacobgroot@users.noreply.github.com>
2023-01-10 12:35:23 +0100

    roster got a fill_schedule

--- classes.py
+++ classes.py
@@ -43,10 +43,17 @@ class Room():
         for day in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']:
             for timeslot in ['9:00 - 11:00', '11:00 - 13:00', '13:00 - 15:00', '15:00 - 17:00']:
                 self.availability[day][timeslot] = True
-
 class Roster():
-    def __init__(self):
-        pass
+    def __init__(self, rooms, type):
+        self.schedule = {}
+        self.rooms = rooms
+        self.type = type # this will be lecture, tutorial or practica
 
-    def assign_tutorial(self):
-        pass
+    def fill_schedule(self, name):
+        # capacity and schedule logic is not important for now
+        for room in self.rooms:
+            for day in room.availity:
+                for timeslot in room.availability[day]:
+                    if room.availability[day][timeslot]:
+                        room.availability[day][timeslot] = False
+                        self.schedule[name] = f"{room.type} in  {room.id} on {room.availability[day]} at {room.availability[day][timeslot]}"
