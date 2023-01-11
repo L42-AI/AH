1ebfcd8272ab6c117a170ad2a74907c6f2cce099
Luka Zaitch <lzaitch@gmail.com>
2023-01-11 17:16:05 +0100

    working dataframe file

--- dataframes.py
+++ dataframes.py
@@ -1,12 +1,6 @@
 from data import *
 from assign import *
-from main import roster
-
-# print(ROOMS)
-# print(STUDENT_COURSES)
-# print(COURSES)
-
-course_list, student_list, rooms = assign(COURSES, ROOMS, STUDENT_COURSES)
+from main import student_list, roster
 
 # Make lists to put into schedule dataframe
 df_list_students = []
@@ -16,22 +10,35 @@ df_list_rooms = []
 df_list_day = []
 df_list_time = []
 
-# For each course
-for i, course in enumerate(course_list):
+abscent_courses = []
 
-    # For the amount of classes in each course
-    for classes in range(1, len(roster.schedule[course.name]) + 1):
+# For each student:
+for i, student in enumerate(student_list):
 
-        # Set the current class
-        current_class = roster.schedule[course.name][f'Class {classes}']
+    # For each course the student is enrolled at
+    for course in student.courses:
 
-        # Add all relevant information ito lists
-        df_list_students.append('Work in Progress')
-        df_list_courses.append(course.name)
-        df_list_type.append(current_class['type'])
-        df_list_rooms.append(current_class['room'])
-        df_list_day.append(current_class['day'])
-        df_list_time.append(current_class['timeslot'])
+        # If the course does not exist in course list save
+        # THIS IS AN ERROR AND NEEDS FIXING
+        if course not in roster.schedule:
+            abscent_courses.append(course)
+            continue
+
+        # For each class:
+        for classes in range(1, len(roster.schedule[course]) + 1):
+
+            # Set the current class
+            current_class = roster.schedule[course][f'Class {classes}']
+
+            # Add all relevant information ito lists
+            df_list_students.append(f'{student.f_name} {student.l_name}')
+            df_list_courses.append(course)
+            df_list_type.append(current_class['type'])
+            df_list_rooms.append(current_class['room'])
+            df_list_day.append(current_class['day'])
+            df_list_time.append(current_class['timeslot'])
+
+print(set(abscent_courses))
 
 # Create schedule
 schedule = pd.DataFrame({'Student': df_list_students,
