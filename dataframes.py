ae422baddf427bf489bb87f5f02173c8e1b294f7
Luka Zaitch <lzaitch@gmail.com>
2023-01-11 16:49:29 +0100

    Imporved Dataframe.py

--- dataframes.py
+++ dataframes.py
@@ -8,6 +8,7 @@ from main import roster
 
 course_list, student_list, rooms = assign(COURSES, ROOMS, STUDENT_COURSES)
 
+# Make lists to put into schedule dataframe
 df_list_students = []
 df_list_courses = []
 df_list_type = []
@@ -15,22 +16,30 @@ df_list_rooms = []
 df_list_day = []
 df_list_time = []
 
+# For each course
 for i, course in enumerate(course_list):
+
+    # For the amount of classes in each course
     for classes in range(1, len(roster.schedule[course.name]) + 1):
 
+        # Set the current class
         current_class = roster.schedule[course.name][f'Class {classes}']
 
-        df_list_students.append(student_list[i].f_name)
+        # Add all relevant information ito lists
+        df_list_students.append('Work in Progress')
         df_list_courses.append(course.name)
         df_list_type.append(current_class['type'])
         df_list_rooms.append(current_class['room'])
         df_list_day.append(current_class['day'])
         df_list_time.append(current_class['timeslot'])
 
+# Create schedule
 schedule = pd.DataFrame({'Student': df_list_students,
                          'Course': df_list_courses,
                          'Activity': df_list_type,
                          'Room': df_list_rooms,
                          'Day': df_list_day,
                          'Time': df_list_time})
-print(schedule['Student'][schedule['Student'] == 'Yanick'])
+
+if __name__ == "__main__":
+    print(schedule)
