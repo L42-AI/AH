0e70a57a38541711121ef44d3b00ea9bb8293dc6
Gid-Git <gidon.quint@ziggo.nl>
2023-01-12 17:24:52 +0100

    count_students is now a function

---
+++ functions/count_students.py
@@ -0,0 +1,17 @@
+def count_students(dataframe):
+    """Counts the amount of students enrolled for each course"""
+
+    list_courses = ['Vak1', 'Vak2', 'Vak3', 'Vak4', 'Vak5']
+    dict_count = {}
+
+    # loop over each row of the dataframe and set the count plus 1
+    for _, row in dataframe.iterrows():
+        for course in list_courses:
+            if row[course] not in dict_count:
+                dict_count[row[course]] = 0
+            dict_count[row[course]] += 1
+    return dict_count
+
+if __name__== "__main__":
+    print(dict_count(STUDENT_COURSES))
+        
