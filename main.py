7f9e12bfe63b01662dd119149dcc742ecc50b576
Jacob Groot <jacobgroot79@gmail.com>
2023-01-12 11:48:15 +0100

    cleaned the main up, some comments and cleaner output

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ from assign import *
 
 course_list, student_list, rooms = assign(COURSES, ROOMS, STUDENT_COURSES)
 
-# THIS CAN PROBABLY BE DELETED LATER BUT IT IS NICE FOR OVERVIEW #
-## first go over every course and see how many timeslots are needed
-### timeslot needed for every lecture, tut, pract
-time_slot_count = 0
-for course in course_list:
-    time_slot_count += course.lectures + course.tutorial_rooms + course.practica_rooms
-# print(time_slot_count)
-
+# create a roster
 roster = Roster(rooms)
+
+# fill the roster
 for course in course_list:
     class_count = 0
     for i in range(course.lectures):
@@ -26,12 +21,22 @@ for course in course_list:
         roster.fill_schedule(course, "practica", class_count, i + 1)
         class_count += 1
 
+# output
 for key in roster.schedule:
+    print("------------------------")
     print(key)
+    print("------------------------")
     print(roster.schedule[key])
-    print("---------------")
+    print()
+    
 
 # # check if students get group
 # for student in student_list:
 #     print(student.tut_group, student.pract_group)
 #     break
+
+## timeslot needed for every lecture, tut, pract
+# time_slot_count = 0
+# for course in course_list:
+#     time_slot_count += course.lectures + course.tutorial_rooms + course.practica_rooms
+#  print(time_slot_count)
