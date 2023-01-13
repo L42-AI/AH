589c114bb74c1caab235ce6182c75b3302c1f8a1
Luka Zaitch <lzaitch@gmail.com>
2023-01-13 14:15:53 +0100

    Small changes to Room file

--- classes/room.py
+++ classes/room.py
@@ -1,15 +1,19 @@
+"""
+This fule includes the class Room which represents the classrooms
+It has one method which is initializing its availablility
+"""
+
 import random
 
 class Room():
-    def __init__(self, room):
-        """ Initialize attributes of class from data """
+    def __init__(self, data):
 
-        # Set attributes
-        self.id = room['Zaalnummber']
-        self.capacity = room['Max. capaciteit']
-        self.availability = {}
+        # Set attributes from data
+        self.id = data['Zaalnummber']
+        self.capacity = data['Max. capaciteit']
 
         # Run initializing funciton
+        self.availability = {}
         self.initialize_availability()
 
     def initialize_availability(self):
@@ -17,8 +21,12 @@ class Room():
 
         # Set lists of days and times
         days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+        # Set possible timeslots
         timeslots = [9, 11, 13, 15]
         timeslots_biggest = [9, 11, 13, 15, 17]
+
+        # Randomly shuffle all lists
         random.shuffle(days)
         random.shuffle(timeslots)
         random.shuffle(timeslots_biggest)
@@ -26,8 +34,10 @@ class Room():
         # For each day:
         for day in days:
 
-            # check if it is the biggest room, if so use other timeslots
+            # Check if it is the biggest room
             if self.id == 'C0.110':
+
+                # use other timeslot list
                 timeslots = timeslots_biggest
 
             # If not yet in availability:
