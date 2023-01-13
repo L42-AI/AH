f26b6c26d65cbbf279e639856c3b4ca23cab71a8
Gid-Git <gidon.quint@ziggo.nl>
2023-01-13 14:18:10 +0100

    count_students and assign so that it is not hard coded

--- functions/assign.py
+++ functions/assign.py
@@ -1,8 +1,11 @@
+# This function 
+
 from classes.course import *
 from classes.room import *
 from classes.student import *
 from functions.count_students import *
 from data import COURSES, ROOMS, STUDENT_COURSES
+import re
 
 
 def assign(COURSES, STUDENT_COURSES, ROOMS):
@@ -10,24 +13,26 @@ def assign(COURSES, STUDENT_COURSES, ROOMS):
     course_list = []
     student_list = []
     rooms = []
-    enrollment = count_students(STUDENT_COURSES)
+    dict_enrollment = count_students(STUDENT_COURSES)
 
     # create an instance for every course
     for _, course in COURSES.iterrows():
-        # Zoeken, sturen en bewegen has "" in the name so can't be found when looking in dict
-        ## therefore explicit argument is needed
-        try:
-            course_list.append(Course(course, enrollment[course['Vak']]))
-        except:
-            course_list.append(Course(course, 42))
+
+        # because the data in vakken.csv is not the same as in student_en_vakken.csv
+        # we choose to put in this regex
+        course_name = re.sub(r',', "", str(course['Vak']))
+
+        # fill in the list with course objects 
+        course_list.append(Course(course, dict_enrollment[course_name]))
 
     for _, student in STUDENT_COURSES.iterrows():
 
-        # fill in the list with student instances
+        # fill in the list with student objects
         student_list.append(Student(student, course_list))
 
     for _, room in ROOMS.iterrows():
 
+        # fill in the list with room objects
         rooms.append(Room(room))
 
     return course_list, student_list, rooms
