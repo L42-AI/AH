f8bff410f1a545e1e0326c34aa6fe6ae6a855971
Luka Zaitch <lzaitch@gmail.com>
2023-01-16 13:31:02 +0100

    Finnish find space dict

--- functions/change_students.py
+++ functions/change_students.py
@@ -1,7 +1,8 @@
 
 def change_students(df, course_list, num=10):
     student_list = find_worst_students(df, num)
-    find_space(course_list)
+    space_dict = find_space(course_list)
+    print(space_dict)
 
 def find_worst_students(df, num=10):
     df.sort_values(['Student Malus'], ascending=True, inplace=True)
@@ -9,20 +10,38 @@ def find_worst_students(df, num=10):
     return worst_students
 
 def find_space(course_list):
+
+    space_dict = {}
+
     for course in course_list:
         max_tut = course.max_std
         max_pra = course.max_std_practica
-        print()
-        print(course)
-        print(course.enrolled)
-        print()
+
         if max_tut > 1:
-            print('tutorial')
-            print(max_tut)
+
             for tutorial in course.tut_group_dict:
-                course.tut_group_dict[tutorial]
+
+                space = int(max_tut - course.tut_group_dict[tutorial])
+
+                if space > 0:
+
+                    if course.name not in space_dict:
+                        space_dict[course.name] = {}
+
+                    space_dict[course.name][f'Tutorial {tutorial}'] = space
+
         if max_pra > 1:
-            print('practica')
-            print(max_pra)
-            print(course.pract_group_dict)
+
+            for practical in course.pract_group_dict:
+
+                space = int(max_pra - course.pract_group_dict[practical])
+
+                if space > 0:
+
+                    if course.name not in space_dict:
+                        space_dict[course.name] = {}
+
+                    space_dict[course.name][f'Practical {practical}'] = space
+
+    return space_dict
 
