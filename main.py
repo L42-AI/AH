956adba03791c513e53a52bdb6b72a09391ddf35
Gid-Git <gidon.quint@ziggo.nl>
2023-01-17 13:00:21 +0100

    change course

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import functions.schedule_fill as schedule
 import functions.assign  as assign
 import functions.dataframes as dataframe
 import functions.change_students as change
+import random
 
 from data import COURSES, STUDENT_COURSES, ROOMS
 
@@ -25,13 +26,56 @@ def initialise():
     # Create a dataframe and export to excel for visual representation
     df = dataframe.schedule_dataframe(Roster, student_list)
 
-    change.change_students(df, course_list)
-
     # Save as malus points
     malus_points = Roster.malus
 
-    return malus_points
+    return malus_points, course_list, student_list, rooms, Roster
+
+def swap_lecture(courses, course, students, roster):
+    
+    # pick a random course to swap with that is not the same as as the course and the new course does have lectures
+    random_course = random.choice([c for c in courses if c != course and c.lectures > 0])
+
+    # make a list of all the lectures
+    lecture_switch = [key for key in roster.schedule[course.name].keys() if "lecture" in key]
+
+    # if there are more then 1 lecture choose a random one
+    if len(lecture_switch) > 1:
+        lecture_switch = random.choice(lecture_switch)
+    
+    # else make from list string again
+    else:
+        lecture_switch = lecture_switch[0]
+    
+    # make a list of all the lectures for the random
+    lecture_random = [key for key in roster.schedule[random_course.name].keys() if "lecture" in key]
+
+    # if there are more then 1 lecture choose a random one
+    if len(lecture_random) > 1:
+        lecture_random = random.choice(lecture_random)
+
+    else:
+        lecture_random = lecture_random[0]
+
+    # hard copy the dictionaries in order to switch the keys
+    dict_switch = roster.schedule[course.name][lecture_switch].copy()
+    dict_random = roster.schedule[random_course.name][lecture_random].copy()
+    keys_to_switch = ['day', 'timeslot', 'room']
+
+    # loop over the keys you want to switch and switch
+    for key in keys_to_switch:
+        roster.schedule[course.name][lecture_switch][key] = dict_random[key]
+        roster.schedule[random_course.name][lecture_random][key] = dict_switch[key]
+
+    
 
 if __name__ == '__main__':
-    baseline = BaselineClass.Baseline()
+    # baseline = BaselineClass.Baseline()
     # baseline.plot_startup()
+
+    malus_points, courses, students, rooms, roster = initialise()
+
+    # print(courses[0].name)
+
+    swap_lecture(courses, courses[0], students, roster)
+
