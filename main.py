53348ba7277732ef2f6afbd68debe2d43fa7af67
Gid-Git <gidon.quint@ziggo.nl>
2023-01-18 11:15:46 +0100

    dele swap functions that were in main and put them into the class

--- main.py
+++ main.py
@@ -35,61 +35,9 @@ def initialise():
 
     return malus_points, course_list, student_list, rooms, Roster
 
-def swap_lectures(courses, course, roster):
-    """
-    This function takes in a list of object courses, a single object course and the object roster.
-    It then takes a random course of the list courses and switches the lecture timeslots of the two.
-    Because the roster schedule is changed, the student timeslots dictionary is changed as well.
-    """
-    
-    # pick a random course to swap with that is not the same as as the course and the new course does have lectures
-    random_course = random.choice([c for c in courses if c != course and c.lectures > 0])
-
-    # get all the lectures
-    lecture_switch = [key for key in roster.schedule[course.name].keys() if "lecture" in key]
-    lecture_random = [key for key in roster.schedule[random_course.name].keys() if "lecture" in key]
-
-    # take a random lecture (if only one it will take the one)
-    lecture_switch = random.choice(lecture_switch)
-    lecture_random = random.choice(lecture_random)
-
-    # define in order to be easier to read and switch key values
-    dict_switch = roster.schedule[course.name][lecture_switch]
-    dict_random = roster.schedule[random_course.name][lecture_random]
-
-    # switch the times in the schedule roster
-    roster.schedule[course.name][lecture_switch] = dict(zip(dict_switch, dict_random.values()))
-    roster.schedule[random_course.name][lecture_random] = dict(zip(dict_random, dict_switch.values()))
-
-def swap_lecture_empty_room(roster, course):
-
-    # get all the lectures in the course and get all the empty rooms
-    lecture_switch = [key for key in roster.schedule[course.name].keys() if "lecture" in key]
-    all_empty_rooms = [key for key in roster.schedule['No course'].keys()]
-
-    # choose a random lecture of that course
-    lecture_switch = random.choice(lecture_switch)
-    random_empty_room = random.choice(all_empty_rooms)
-
-    # define in order to be easier to read and to be able to switch keys and values of the dict
-    dict_switch = roster.schedule[course.name][lecture_switch]
-    dict_random = roster.schedule['No course'][random_empty_room]
-    
-    # switch the times in the schedule roster
-    roster.schedule[course.name][lecture_switch] = dict(zip(dict_switch, dict_random.values()))
-    roster.schedule['No course'][random_empty_room] = dict(zip(dict_random, dict_switch.values()))
-
-
 if __name__ == '__main__':
     # baseline = BaselineClass.Baseline()
     # baseline.plot_startup()
 
     malus_points, courses, students, rooms, roster = initialise()
     
-    # print(courses[0].name)
-
-    # swap_lectures(courses, courses[0], roster)
-    # print(roster.schedule['No course'])
-    
-    swap_lecture_empty_room(roster, courses[0])
-
