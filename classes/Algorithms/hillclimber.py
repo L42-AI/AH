54649e6674978f1a41923422198cb8a3c7a137a5
Luka Zaitch <lzaitch@gmail.com>
2023-01-21 19:49:52 +0100

    Split off simulated annealing

--- classes/Algorithms/hillclimber.py
+++ classes/Algorithms/hillclimber.py
@@ -1,5 +1,4 @@
 import classes.Algorithms.mutate as MutateClass
-import math
 import copy
 import random
 
@@ -124,44 +123,3 @@ class HC_SwapBadTimeslots_DoubleClasses(HillClimber):
 
     def step_method(self, M):
         M.swap_bad_timeslots()
-
-
-
-class Simulated_Annealing(HillClimber):
-
-    def replace_roster(self, T):
-
-            if self.best_malus_score > self.current_malus_points:
-                self.best_roster = self.current_roster
-                self.best_malus_score = self.current_malus_points
-            
-            else: 
-                p = math.exp(- (self.best_malus_score - self.current_malus_points) / T)
-                randint = random.random()
-                if randint <= p:
-                    self.best_roster = self.current_roster
-                    self.best_malus_score = self.current_malus_points
-
-""" Step method bestaat niet meer, zit in lecture swap? """
-# class SA_LectureLocate(Simulated_Annealing):
-#     def step_method(self, M):
-#         M.swap_lecture_empty_room()
-
-class SA_LectureSwap(Simulated_Annealing):
-    def step_method(self, M):
-
-        # Take a random state to pass to function
-        state = random.choice((True, False))
-        M.swap_random_lessons(state)
-
-    def get_name(self):
-        return "Lesson Swapped"
-
-class SA_StudentSwap(Simulated_Annealing):
-    def step_method(self, M):
-        M.swap_2_students()
-
-    def get_name(self):
-        return "Students Swapped"
-
-""" Missen nog beide swap bad timeslots """
