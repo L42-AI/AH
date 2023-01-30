e86ecf0eb053dc393e3c0a6b33ea77bde89c7676
Gid-Git <gidon.quint@ziggo.nl>
2023-01-30 16:10:01 +0100

    done with the different multipliers

--- data/look_at_data.py
+++ data/look_at_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('Normal Hillclimber geen multiplier 30 keer.csv')
+# df = pd.read_csv('Normal Hillclimber geen multiplier 30 keer.csv')
 
 # threshold = 400
 # max_iteration = 330
@@ -10,14 +10,21 @@ df = pd.read_csv('Normal Hillclimber geen multiplier 30 keer.csv')
 # df_2 = df[df['Total Malus'] <= threshold]
 # df_2 = df_2[df_2['Iteration'] <= max_iteration]
 
-sns.lineplot(data=df, x='Unnamed: 0', y='Total Malus')
-plt.xlabel('Iterations')
-plt.title('Run the Hillclimber 30 times')
+# sns.lineplot(data=df, x='Unnamed: 0', y='Total Malus')
+# plt.xlabel('Iterations')
+# plt.title('Run the Hillclimber 30 times')
 # sns.lineplot(data=df_2, x='Iteration', y='Total Malus')
 # plt.title('Average of the 30 Hillclimbers')
 
 # plt.show()
 
-plt.savefig('Plot show all 30 climbers.png', dpi=1000)
+# plt.savefig('Plot show all 30 climbers.png', dpi=1000)
 
+# print(df)
+
+df = pd.read_csv('Different Multipliers.csv')
+
+sns.lineplot(data=df, x='Unnamed: 0', y='Total Malus')
+
+plt.show()
 # print(df)
