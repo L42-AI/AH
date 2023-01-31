07ed9d69d03ed542d6a23bf454ef3a5e89ef5c62
Luka Zaitch <lzaitch@gmail.com>
2023-01-31 21:24:25 +0100

    Add functionality and widgets

--- classes/GUI/init_GUI.py
+++ classes/GUI/init_GUI.py
@@ -14,7 +14,7 @@ class App(customtkinter.CTk):
        
         # configure window
         self.title("Scheduly")
-        self.geometry(f"{250}x{350}")
+        self.geometry(f"{250}x{450}")
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(0, weight=1)
@@ -25,32 +25,70 @@ class App(customtkinter.CTk):
         self.toggle_frame = customtkinter.CTkFrame(self)
         self.toggle_frame.grid(row=0, column=0, rowspan=2, padx=20, pady=20, sticky="nsew")
         self.toggle_frame.grid_columnconfigure(0, weight=1)
-        self.toggle_frame.grid_rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11), weight=1)
-    
-        
+        self.toggle_frame.grid_rowconfigure((0,1,2,3,4,5,6,7), weight=1)
 
         self.label_experiment = customtkinter.CTkLabel(master=self.toggle_frame, text="Experiments:", font=customtkinter.CTkFont(size=15, weight='bold'))
         self.label_experiment.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
 
-        self.experiment1_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 1', font=customtkinter.CTkFont(size=15))
+        self.experiment1_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 1', font=customtkinter.CTkFont(size=15), command=self.experiment1_switch_click)
         self.experiment1_switch.grid(row=1, column=0, pady=10, padx=20, sticky="nsew")
 
-        self.experiment2_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 2', font=customtkinter.CTkFont(size=15))
+        self.experiment2_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 2', font=customtkinter.CTkFont(size=15), command=self.experiment2_switch_click)
         self.experiment2_switch.grid(row=2, column=0, pady=10, padx=20, sticky="nsew")
-        
-        self.experiment3_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 3', font=customtkinter.CTkFont(size=15))
+
+        self.experiment3_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 3', font=customtkinter.CTkFont(size=15), command=self.experiment3_switch_click)
         self.experiment3_switch.grid(row=3, column=0, pady=10, padx=20, sticky="nsew")
 
+        self.experiment4_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 3', font=customtkinter.CTkFont(size=15), command=self.experiment4_switch_click)
+        self.experiment4_switch.grid(row=4, column=0, pady=10, padx=20, sticky="nsew")
+
+        self.experiment5_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Experiment 3', font=customtkinter.CTkFont(size=15), command=self.experiment5_switch_click)
+        self.experiment5_switch.grid(row=5, column=0, pady=10, padx=20, sticky="nsew")
+
+
         self.create_own_exp_switch = customtkinter.CTkCheckBox(master=self.toggle_frame, text='Create your own:', font=customtkinter.CTkFont(size=15), command=self.create_own_experiment)
-        self.create_own_exp_switch.grid(row=4, column=0, pady=10, padx=20, sticky="nsew")
-        
-        
+        self.create_own_exp_switch.grid(row=6, column=0, pady=10, padx=20, sticky="nsew")
 
         """ Setup Generate Button """
 
         # Generate Button
         self.generate_button = customtkinter.CTkButton(master=self, text="Generate", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), command=self.generate)
-        self.generate_button.grid(row=8, column=0, padx=20, pady=10, sticky="ew")
+        self.generate_button.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
+
+    def experiment1_switch_click(self) -> None:
+        self.experiment2_switch.deselect()
+        self.experiment3_switch.deselect()
+        self.experiment4_switch.deselect()
+        self.experiment5_switch.deselect()
+        self.create_own_exp_switch.deselect()
+
+    def experiment2_switch_click(self) -> None:
+        self.experiment1_switch.deselect()
+        self.experiment3_switch.deselect()
+        self.experiment4_switch.deselect()
+        self.experiment5_switch.deselect()
+        self.create_own_exp_switch.deselect()
+
+    def experiment3_switch_click(self) -> None:
+        self.experiment1_switch.deselect()
+        self.experiment2_switch.deselect()
+        self.experiment4_switch.deselect()
+        self.experiment5_switch.deselect()
+        self.create_own_exp_switch.deselect()
+
+    def experiment4_switch_click(self) -> None:
+        self.experiment1_switch.deselect()
+        self.experiment2_switch.deselect()
+        self.experiment3_switch.deselect()
+        self.experiment5_switch.deselect()
+        self.create_own_exp_switch.deselect()
+
+    def experiment5_switch_click(self) -> None:
+        self.experiment1_switch.deselect()
+        self.experiment2_switch.deselect()
+        self.experiment3_switch.deselect()
+        self.experiment4_switch.deselect()
+        self.create_own_exp_switch.deselect()
 
     def create_own_experiment(self):
 
@@ -64,9 +102,10 @@ class App(customtkinter.CTk):
 
             self.expand_gui()
             self.create_own_exp_frame = customtkinter.CTkFrame(self)
-            self.create_own_exp_frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+            self.create_own_exp_frame.grid(row=0, column=1, rowspan=2, padx=20, pady=20, sticky="nsew")
             self.create_own_exp_frame.grid_columnconfigure(0, weight=1)
-            self.create_own_exp_frame.grid_rowconfigure((0,1,2,3,4,5,6,7,8,9,10,11), weight=1)
+            self.create_own_exp_frame.grid_rowconfigure((0,1,2,3,4,5,6,7), weight=1)
+
             self.label_initialization = customtkinter.CTkLabel(master=self.create_own_exp_frame, text="Choose settings:", font=customtkinter.CTkFont(size=15, weight='bold'))
             self.label_initialization.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
 
@@ -75,35 +114,42 @@ class App(customtkinter.CTk):
 
             self.hill_climbing_switch = customtkinter.CTkSwitch(master=self.create_own_exp_frame, text='Hill Climbing', font=customtkinter.CTkFont(size=15), command=self.hillclimber_switch_click)
             self.hill_climbing_switch.grid(row=6, column=0, pady=10, padx=20, sticky="n")
+
+            self.generate_button.grid(row=2, column=0, columnspan=2, padx=20, pady=10, sticky="nsew")
         else:
-            
+
             self.hill_climbing_switch.destroy()
+
             try:
                 self.annealing_switch.destroy()
             except:
                 pass
+
             self.greedy_switch.deselect()
+
             try:
                 self.greedy_switch_click()
             except:
                 pass
+
             self.label_initialization.destroy()
             self.greedy_switch.destroy()
-            
+
             self.collaps_gui()
             self.create_own_exp_frame.destroy()
+            self.generate_button.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
 
     def expand_gui(self):
-        self.geometry(f"{500}x{550}")
+        self.geometry(f"{500}x{450}")
 
     def expand_gui_greedy(self):
         self.geometry(f'{500}x{550}')
 
     def expand_gui_hilli(self):
-        self.geometry(f'{500}x{650}')
+        self.geometry(f'{500}x{550}')
 
     def collaps_gui(self):
-        self.geometry(f'{250}x{350}')
+        self.geometry(f'{250}x{450}')
 
     def run(self) -> None:
         self.mainloop()
@@ -116,12 +162,29 @@ class App(customtkinter.CTk):
             self.expand_gui_hilli()
             self.annealing_switch = customtkinter.CTkSwitch(master=self.create_own_exp_frame, text='Sim. Annealing', font=customtkinter.CTkFont(size=15))
             self.annealing_switch.grid(row=7, column=0, padx=20, pady=10, sticky="nsew")
+
+
+            self.duration_box = customtkinter.CTkComboBox(self.create_own_exp_frame,
+                                                        values=["1", "5", "10", "15", "30", "45", "60", "120", "180"])
+            self.duration_box.grid(row=8, column=0, padx=20, pady=10, sticky="nsew")
+            self.duration_box.set("Duration (Mins.)")
+
+            core_arangments = self.__gen_core_assignment_list()
+
+            self.core_assingment_box = customtkinter.CTkComboBox(self.create_own_exp_frame,
+                                                        values=core_arangments)
+            self.core_assingment_box.grid(row=9, column=0, padx=20, pady=10, sticky="nsew")
+            self.core_assingment_box.set("HC Distribution")
+
+            self.mode_menu = customtkinter.CTkOptionMenu(self,
+                                       values=["Seq", "Multi","Genetic Seq", "Genetic Multi"])
+            self.mode_menu.grid(row=10, column=0, padx=20, pady=10, sticky="nsew")
+
         else:
             self.expand_gui_greedy()
             self.annealing_switch.destroy()
 
     def greedy_switch_click(self) -> None:
-        
 
         state_greedy = self.greedy_switch.get()
 
@@ -145,10 +208,6 @@ class App(customtkinter.CTk):
             self.popular_switch.destroy()
             self.popular_own_day_switch.destroy()
             self.difficult_students_switch.destroy()
-
-    def run_experiment(self):
-        if self.experiment1_switch.get():
-            self.run_exp_1()
 
     def run_exp_1(self) -> None:
         # Set setting for initialization plot or optimalization
@@ -184,7 +243,7 @@ class App(customtkinter.CTk):
                                      hill_climber_iters, i)
         # Plot Funtion
 
-    def experiment2_switch_click(self) -> None:
+    def run_exp_2(self) -> None:
         # Set setting for initialization plot or optimalization
 
         capacity = False
@@ -218,7 +277,7 @@ class App(customtkinter.CTk):
                                      hill_climber_iters, i)
         # Plot Funtion
 
-    def experiment3_switch_click(self) -> None:
+    def run_exp_3(self) -> None:
         # Set setting for initialization plot or optimalization
 
         capacity = False
@@ -252,7 +311,7 @@ class App(customtkinter.CTk):
                                      hill_climber_iters, i)
         # Plot Funtion
 
-    def experiment4_switch_click(self) -> None:
+    def run_exp_4(self) -> None:
         capacity = False
 
         popular = False
@@ -286,7 +345,7 @@ class App(customtkinter.CTk):
                                      experiment, mode, core_assignment,
                                      hill_climber_iters, i)
 
-    def experiment5_switch_click(self) -> None:
+    def run_exp_5(self) -> None:
         capacity = False
 
         popular = False
@@ -339,13 +398,32 @@ class App(customtkinter.CTk):
 
     def generate(self) -> None:
 
-        # Extract state_data from GUI
-        settings = self.__set_data()
+        exp1 = self.experiment1_switch.get()
+        exp2 = self.experiment2_switch.get()
+        exp3 = self.experiment3_switch.get()
+        exp4 = self.experiment4_switch.get()
+        exp5 = self.experiment5_switch.get()
+        own_exp = self.create_own_exp_switch.get()
 
-        # Destroy window
-        self.destroy()
+        if exp1:
+            self.run_exp_1()
+        elif exp2:
+            self.run_exp_2()
+        elif exp3:
+            self.run_exp_3()
+        elif exp4:
+            self.run_exp_4()
+        elif exp5:
+            self.run_exp_5()
+        elif own_exp:
 
-        self.__run_algorithm(settings)
+            # Extract state_data from GUI
+            settings = self.__set_data()
+
+            # Destroy window
+            self.destroy()
+
+            self.__run_algorithm(settings)
 
     def finish(self, student_list):
 
@@ -404,3 +482,12 @@ class App(customtkinter.CTk):
 
         if not visualize:
             G.optimize()
+
+    def __gen_core_assignment_list(self) -> list:
+        core_arragments = []
+        for a in range(4):
+            for b in range(4):
+                for c in range(4):
+                    for d in range(4):
+                        core_arragments.append(f'{a}{b}{c}{d}')
+        return core_arragments
