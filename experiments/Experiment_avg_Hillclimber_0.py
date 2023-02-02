e0110b9549d92de4e241cac8d1928facfe61f4cc
Gid-Git <gidon.quint@ziggo.nl>
2023-02-02 15:30:36 +0100

    also show the zoomed in data

--- experiments/Experiment_avg_Hillclimber_0.py
+++ experiments/Experiment_avg_Hillclimber_0.py
@@ -9,19 +9,23 @@ if __name__ == '__main__':
     This csv file has as columns Swap Students as well, however the Swap Random Seminar Hillclimber was only used.
     This is simply a mistake of putting in the wrong headers. The data is still correct.
     """
+    
+    zoomed = False
+
 
     # load the data into a dataframe
     df = pd.read_csv('Testing Hillclimber_0 30 times.csv')
 
-    # cut so the plot is prittier
-    df_2 = df[df['Iteration.1'] <= 400]
-    df_2 = df_2[df_2['list_iterations'] <= 300]
+    if zoomed:
+        # cut so the plot is prittier
+        df = df[df['Iteration.1'] <= 400]
+        df = df_2[df_2['list_iterations'] <= 300]
     
     # plot and save the graph
     plt.figure(figsize=(10, 4))
     plt.style.use('seaborn-whitegrid')
 
-    sns.lineplot(data=df_2, x='list_iterations', y='Iteration.1')
+    sns.lineplot(data=df, x='list_iterations', y='Iteration.1')
     plt.title('Average of Swap Random Seminars')
 
 
